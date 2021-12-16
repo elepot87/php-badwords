43383dd26a3ec5fcae2e6dbac11ed9cf2b850833
elepot87 <eleonorapotente@gmail.com>
2021-12-16 16:05:56 +0100

    sostituzione parola inserite da utente con nel paragrafo e nuova lunghezza

--- index.php
+++ index.php
@@ -8,19 +8,29 @@
 </head>
 <body>
 <?php
-$sentence = 'Non conosco il linguaggio PHP';
-$newSentence = $_GET['frase'];
+$sentence = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ac lacus magna. Integer porttitor arcu vitae ipsum volutpat, consectetur pretium massa mattis. Mauris hendrerit dolor vel est convallis fringilla. Sed eget interdum quam. Cras elementum finibus purus eu consectetur. Praesent scelerisque libero ut varius luctus. Fusce volutpat metus sed iaculis fringilla. Nam a sem id est ornare malesuada. Sed dictum arcu at lectus varius pellentesque.';
+$userWord = $_GET['frase'];
+$newSentence = str_replace($userWord, "***", $sentence);
 ?>
     <h1>Ciao sono Eleonora</h1>
     <h2>
         <?php echo $sentence; ?>
     </h2>
-    <p>Lunghezza stringa:
+    <p>Lunghezza paragrafo:
         <?php echo strlen($sentence);?>
     </p>
     <h2>
-        La nuova frase è <?php echo $newSentence; ?>
+        La nuova frase è <?php echo $userWord; ?>
     </h2>
+
+    <h2>
+        Sostituzione paragrafo precedente: <br>
+        <?php echo $newSentence; ?>
+    </h2>
+
+    <p>Lunghezza nuovo paragrafo:
+        <?php echo strlen($newSentence);?>
+    </p>
    
 </body>
 </html>
